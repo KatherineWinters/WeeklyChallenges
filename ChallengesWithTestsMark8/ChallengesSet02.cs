932b8215d4b39b93fb0486406c9e228011b13ae4
Katherine Winters <goble.katherine@gmail.com>
2021-12-28 15:51:49 -0600

    initial commit

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -8,7 +8,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            if (CharacterIsALetter(c))
+            if (char.IsLetter(c))
             {
                 return true;
             }
@@ -20,8 +20,7 @@ namespace ChallengesWithTestsMark8
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            var x = vals.Length;
-            if (x % 2 == 0)
+            if (vals.Length % 2 == 0)
             {
                 return true;
             }
@@ -43,104 +42,96 @@ namespace ChallengesWithTestsMark8
             }
         }
 
-        public bool IsNumberOdd(int num)
+        public bool IsNumberOdd(int number)
         {
-            if (num % 2 == 0)
+            if (number % 2 != 0)
             {
-                return false;
+                return true;
             }
             else
             {
-                return true;
+                return false;
             }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            return numbers.Min() + numbers.Max() / 2;
+            if(numbers == null)
+            {
+                return 0;
+            }
+
+            if(numbers.Count() == 0)
+            {
+                return 0;
+            }
+
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            int x = str1.Length;
-            int y = str2.Length;
-
-            if (x < y)
+            if (str1.Length < str2.Length)
             {
-                return x;
+                return str1.Length;
             }
             else
             {
-                return y;
+                return str2.Length;
             }
         }
 
         public int Sum(int[] numbers)
         {
-            int sum = 0;
-
-            if (numbers == null)
+            if(numbers == null)
             {
                 return 0;
             }
+            var sum = 0;
+
             for (int i = 0; i < numbers.Length; i++)
             {
-                if (numbers.Length >= 0)
-                {
-                   sum += numbers[i];
-                }
-                else
-                {
-                    return sum = 0;
-                }
-                    return sum;
+                sum += numbers[i];
             }
+                 return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            if (numbers == null)
+           if( numbers == null)
             {
-             return 0;
+                return 0;
             }
-            int sum = 0;
-            for (int i = 0; numbers.Length > 0; i++)
-                 if (i % 2 == 0)
-                 {
-                    return  sum += 1;
-                 }
-            else
-                {
-                    return sum;
-                }
+            var sum = 0;
 
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 ==0)
+                {
+                    sum += numbers[i];
+                }
+            }
+                return sum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            if (numbers == null)
-            {
-                return false; 
-            }
-            if (numbers.Count % 2 == 0)
+           if(numbers ==null)
             {
                 return false;
             }
-            else
-            {
-                return true;
-            }
+                return (numbers.Sum() % 2 != 0);
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            if (number >= 0)
+            if (number <= 0)
             {
-                return number / 2;
+                return 0;
             }
             else
             {
-                return 0;
+                return number / 2;
             }
         }
     }
